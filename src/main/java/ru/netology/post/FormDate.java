14299ddec15773029a2e304c11634c3173d29c84
Oleg50sev <oleg_t_71@mail.ru>
2022-03-04 19:43:30 +0300

    Initial commit

---
+++ src/main/java/ru/netology/post/FormDate.java
@@ -0,0 +1,7 @@
+package ru.netology.post;
+
+public class FormDate {
+    public String day; // поле для дня рождения
+    public String month; // поле для меяца рождения
+    public String year; // поле для года рождения
+}
---
+++ src/main/java/ru/netology/post/Post.java
@@ -0,0 +1,11 @@
+package ru.netology.post;
+
+public class Post {
+    public String name; // поле для имени
+    public String passport; // поле для паспорта
+    public String patronymic; // поле для Отчества
+    public String phone; // поле для номера телефона
+    public String surname; // поле для номера Фамилии
+    public boolean subscription; // поле о согласии на смс информировании
+    public FormDate birthday; // поле для дня рождения
+}
